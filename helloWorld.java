f949f7c91330715e335505d283b946bdbba6e34f
Natotela1 <totimabela@gmail.com>
2021-07-13 17:49:19 +0200

    Modify your program to accept input from the user and then print out the

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,11 @@
+import java.util.*;	// Import the scanner Class
 public class helloWorld {
 	public static void main(String[] args) {
 		System.out.println("Git is Awesome!!");
+		
+		Scanner in = new Scanner(System.in);	// Scanner object
+		System.out.println("Dont you Agree?");	
+		String opinion = in.nextLine();	// Read user input
+		System.out.println("Your opinion " + opinion);	// Output input
 	}
 }
